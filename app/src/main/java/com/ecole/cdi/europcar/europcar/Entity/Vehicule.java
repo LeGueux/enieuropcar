fbfe51a9a79ea3b872fcb749b2e4a8c69b46d476
LeGueux <alexandre-bat@hotmail.fr>
2018-04-09 12:11:19 +0200

    vehcule entity

--- app/src/main/java/com/ecole/cdi/europcar/europcar/Entity/Vehicule.java
+++ app/src/main/java/com/ecole/cdi/europcar/europcar/Entity/Vehicule.java
@@ -1,4 +1,49 @@
 package com.ecole.cdi.europcar.europcar.Entity;
 
 public class Vehicule {
+
+    private int id;
+    private String marque, modele, immatriculation;
+
+    public Vehicule() {
+    }
+
+    public Vehicule(int id, String marque, String modele, String immatriculation) {
+        this.id = id;
+        this.marque = marque;
+        this.modele = modele;
+        this.immatriculation = immatriculation;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getMarque() {
+        return marque;
+    }
+
+    public void setMarque(String marque) {
+        this.marque = marque;
+    }
+
+    public String getModele() {
+        return modele;
+    }
+
+    public void setModele(String modele) {
+        this.modele = modele;
+    }
+
+    public String getImmatriculation() {
+        return immatriculation;
+    }
+
+    public void setImmatriculation(String immatriculation) {
+        this.immatriculation = immatriculation;
+    }
 }
